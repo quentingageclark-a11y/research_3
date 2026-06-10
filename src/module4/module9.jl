2ae2324619d86d2a17d65810c75d87679c357e62
quentingageclark-a11y <quentin.gage.clark@gmail.com>
2026-06-10 18:02:41 -0500

    Update section and order numbers in module9.jl

--- src/module4/module9.jl
+++ src/module4/module9.jl
@@ -3,10 +3,10 @@
 
 #> [frontmatter]
 #> chapter = 4
-#> section = 9
-#> order = 9
+#> section = 10
+#> order = 10
 #> image = "https://raw.githubusercontent.com/fonsp/Pluto.jl/580ab811f13d565cc81ebfa70ed36c84b125f55d/demo/plutodemo.gif"
-#> title = "9 Data Analysis Notebook"
+#> title = "10 Data Analysis Notebook"
 #> tags = ["module4", "track_julia", "track_material", "Pluto", "PlutoUI"]
 #> layout = "layout.jlhtml"
 
