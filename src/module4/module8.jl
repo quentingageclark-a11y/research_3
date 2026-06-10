d57679d4ee7a23de9dd0affd549ef9553b37d479
quentingageclark-a11y <quentin.gage.clark@gmail.com>
2026-06-10 17:18:12 -0500

    Add frontmatter for chapter 4, section 8

--- src/module4/module8.jl
+++ src/module4/module8.jl
@@ -1,6 +1,15 @@
 ### A Pluto.jl notebook ###
 # v0.20.27
 
+#> [frontmatter]
+#> chapter = 4
+#> section = 8
+#> order = 8
+#> image = "https://raw.githubusercontent.com/fonsp/Pluto.jl/580ab811f13d565cc81ebfa70ed36c84b125f55d/demo/plutodemo.gif"
+#> title = "8 Data Analysis Notebook"
+#> tags = ["module4", "track_julia", "track_material", "Pluto", "PlutoUI"]
+#> layout = "layout.jlhtml"
+
 using Markdown
 using InteractiveUtils
 
