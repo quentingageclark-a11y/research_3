332c3b9bc588a9a135cc7b83d826e601e93ecad0
quentingageclark-a11y <quentin.gage.clark@gmail.com>
2026-06-10 16:51:37 -0500

    added in notebook 2 mod 1

--- src/module4/module1.jl
+++ src/module4/module1.jl
@@ -1,6 +1,16 @@
 ### A Pluto.jl notebook ###
 # v0.20.27
 
+#> [frontmatter]
+#> chapter = 4
+#> section = 2
+#> order = 2
+#> image = "https://raw.githubusercontent.com/fonsp/Pluto.jl/580ab811f13d565cc81ebfa70ed36c84b125f55d/demo/plutodemo.gif"
+#> title = "Second Data Analysis Notebook"
+#> tags = ["module4", "track_julia", "track_material", "Pluto", "PlutoUI"]
+#> layout = "layout.jlhtml"
+
+
 using Markdown
 using InteractiveUtils
 
