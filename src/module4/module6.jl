c245180c669868986894ec8386f6b59773c25805
quentingageclark-a11y <quentin.gage.clark@gmail.com>
2026-06-10 17:25:35 -0500

    Update module6.jl

--- src/module4/module6.jl
+++ src/module4/module6.jl
@@ -3,10 +3,10 @@
 
 #> [frontmatter]
 #> chapter = 4
-#> section = 6
-#> order = 6
+#> section = 7
+#> order = 7
 #> image = "https://raw.githubusercontent.com/fonsp/Pluto.jl/580ab811f13d565cc81ebfa70ed36c84b125f55d/demo/plutodemo.gif"
-#> title = "6 Data Analysis Notebook"
+#> title = "7 Data Analysis Notebook"
 #> tags = ["module4", "track_julia", "track_material", "Pluto", "PlutoUI"]
 #> layout = "layout.jlhtml"
 
